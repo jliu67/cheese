14fc89be46364847ba24752fff7d85729a28b51d
jliu67 <166252253+jliu67@users.noreply.github.com>
2026-08-30 20:22:11 -0600

    Delete qre-two-asset-v12.0.0 directory

--- qre-two-asset-v12.0.0/run_engine.py
+++
@@ -1,5 +0,0 @@
-from qre_two_asset.cli import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
--- qre-two-asset-v12.0.0/src/qre_two_asset/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Two-asset Quant Regime Engine."""
-
-from .config import EngineConfig, load_config
-
-__all__ = ["EngineConfig", "load_config"]
-__version__ = "12.0.0"
